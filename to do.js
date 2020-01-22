79e1c6d898758cd80003e96f3fb6437877f8cfc8
Rahul <rahulramesh26@yahoo.com>
2020-01-22 23:11:32 +0530

    added "delete" function

--- to do.js
+++ to do.js
@@ -1,16 +1,34 @@
-var todo=[];
-var input=prompt("what would you like to do?");
+var todo = [];
+var input = prompt("what would you like to do?");
 
-while(input!=="quit"){
-    if(input==="list"){
-        console.log(todo);
-    }
-    else if(input==="new"){
-        var newToDo=prompt("what would you like to add?");
-        todo.push(newToDo);
+while (input !== "quit") {
+    if (input === "list") {
+         listTodo();
+    } else if (input === "new") {
+         newTOdo();
+    } else if (input="delete"){
+     deeteTodo()
         
     }
-    var input=prompt("what would you like to do?");
-
+    var input = prompt("what would you like to do?");
 }
 console.log("ok then, I guess i will see you some day!!")
+
+function newTOdo(){
+    var newToDo = prompt("what would you like to add?");
+    todo.push(newToDo);
+    console.log("added new todo")
+
+}
+function deeteTodo(){
+    var index=prompt("Enter the index to delete");
+    todo.splice(index,1);
+    console.log("deleted todo")
+}
+function listTodo(){
+    console.log("*******")
+    todo.forEach(function(todos,i){
+        console.log(i + ": " + todos);
+    });
+    console.log("*******")
+}
